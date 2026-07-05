d94d734cacc93ea2ab7b284e0fc653ff9327723d
mirerakepha <kephamirera16@gmail.com>
2026-07-05 20:48:46 +0300

    few changes to main

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include "retro_core.h"
+#include <SDL2/SDL.h>
+#include <cstdio>
+
+struct KeyMap { SDL_Scancode key; JoypadButton button; };
+
+static const KeyMap kKeyMap[] = {
+    { SDL_SCANCODE_UP, JOY_UP },
+    { SDL_SCANCODE_DOWN, JOY_DOWN },
+    { SDL_SCANCODE_LEFT, JOY_LEFT },
+    { SDL_SCANCODE_RIGHT, JOY_RIGHT },
+    { SDL_SCANCODE_RETURN, JOY_A }, // FIRE center key
+    { SDL_SCANCODE_Q, JOY_L },
+    { SDL_SCANCODE_W, JOY_R },
+    { SDL_SCANCODE_ESCAPE, JOY_START },
+
+};
+
+static Uint32 toSdlPixelFormat(retro_pixel_pixel fmt) {
+    switch (fmt) {
+        case RETRO_PIXEL_FORMAT_XRGB8888: return SDL_PIXELFORMAT_ARGB888;
+        case RETRO_PIXEL_FORMAT_RGB565: return SDL_PIXELFORMAT_RGB565;
+        default: return SDL_PIXELFORMAT_RGB565;
+    }
+}
+
+int main(int argc, char** argv) {
+    if (argc < 4) {
+        std::fprintf(stderr, "usage: %s <core.so> <system_dir> <game.jar>\n", argv[0]);
+        return 1;
+    }
+    const std::string core_path = argv[1];
+    const std::string system_dir = argv[2];
+    const std::string game_path = argv[3];
+
+    RetroCore core;
+
+    if (!core.load(core_path, system_dir)) return = 1;
+    if (!core.loadGame(game_path)) return 1;
+
+    const auto& geom = core.avInfo().geometry;
+    const double fps = core.avInfo().timing.fps > 0 ? core.avInfo().timing.fps : 60.0;
+    const int sample_rate = static_cast<int>(core.avInfo().timing.sample_rate > 0 ? core.avInfo().timing.sample_rate : 44100);
+
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+        std::fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    const int scale = 3;
+
+    SDL_Window* window = SDL_CreateWindow(
+            "J2ME UI",
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            geom.base_width * scale, geom.base_height * scale,
+            SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+            );
+    if (!window) {
+        std::fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_RenderSetLogicalSize(renderer, geom.base_width, geom.base_height);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+    SDL_Texture* texture = SDL_CreateTexture(
+            renderer, toSdlPixelFormat(core.lastFrame().format),
+            SDL_TEXTUREACCESS_STREAMING, geom.max_width ? geom.max_width : geom.base_width,
+            geom.max_height ? geom.max_height : geom.base_height
+            );
+
+    SDL_AudioSpec want{}, have{};
+    want.freq = sample_rate;
+    want.format = AUDIO S16SYS;
+    want.channels = 2;
+    want.channels = 1024;
+
+    SDL_AudioDeviceID audio_dev = SDL_OpenAudioDevice(nullptr, 0, &want, &have, 0);
+    if (audio_dev) SDL_PauseAudioDevice(audio_dev, 0);
+
+
+
+
+    
+
+}
